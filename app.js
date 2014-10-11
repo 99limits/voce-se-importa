4c2b71946ea3f3c64df0beb1c4d5448cb31b77e9
Luiz Faias <luizfaias@gmail.com>
2014-10-11 23:23:26 +0100

    apagando códigos iniciais desnecessários

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ var path = require('path');
 var routes_index = require('./routes/index');
 
 var app = express();
-
-app.set('port', (process.env.PORT || 3000))
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(3000);
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
