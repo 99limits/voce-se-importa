394b035288bbc12f9fb5eb80586253ed7477f515
Daniel Faria Gomes <danielfariati@gmail.com>
2014-10-17 19:44:03 -0300

    adicionando node

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var express = require('express')
-var path = require('path');
+var express = require('express'),
+    path = require('path'),
+    routes_index = require('./routes/index'),
+    mongo = require('mongodb');
+    monk = require('monk');
+    db = monk(process.env.MONGOHQ_URL || 'localhost:27017/voceseimporta'),
+    app = express();
 
-var routes_index = require('./routes/index');
-
-var app = express();
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'));
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+})
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes_index);
