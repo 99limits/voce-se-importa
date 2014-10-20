30b10c83310e733329180d7d528d60a5e772e1b6
Daniel Faria Gomes <danielfariati@gmail.com>
2014-10-20 19:43:19 -0200

    adicionando página de instituições dinâmica

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,17 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/:tag', function(req, res) {
+  var tag = req.params.tag,
+      db = req.db,
+      collection = db.get('instituicoes');
+
+  collection.find({ tags:  tag }, {}, function(e, instituicoes) {
+    res.render('instituicao', {
+      tag: tag,
+      instituicao: instituicoes.length ? instituicoes[0] : null
+    });
+  });
+});
+
 module.exports = router;
